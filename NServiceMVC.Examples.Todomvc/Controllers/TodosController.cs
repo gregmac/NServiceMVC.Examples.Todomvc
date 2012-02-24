7ffdc04c662b24e118fec9c6161ed4c5af1b6dda
gregmac <github@list.mtechsolutions.ca>
2012-02-24 00:56:00 -0500

    * Add Todo model
    * Add hardcoded GET /todos REST method

---
+++ NServiceMVC.Examples.Todomvc/Controllers/TodosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NServiceMVC;
+using AttributeRouting;
+
+namespace NServiceMVC.Examples.Todomvc.Controllers
+{
+    public class TodosController : ServiceController
+    {
+        [GET("todos")]
+        public IEnumerable<Models.Todo> Index()
+        {
+            return new List<Models.Todo>() {
+                new Models.Todo() {
+                    Text = "Item1",
+                },
+                new Models.Todo() {
+                    Text = "Item2",
+                    Done = true,
+                }
+            };
+        }
+    }
+}
---
+++ NServiceMVC.Examples.Todomvc/Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NServiceMVC.Examples.Todomvc.Models
+{
+    public class Todo
+    {
+        public Todo()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public bool Done { get; set; }
+        public int Order { get; set; }
+    }
+}
