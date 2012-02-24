9513897c45a07b8feea66b613e350e40d64c1c69
gregmac <github@list.mtechsolutions.ca>
2012-02-24 01:22:32 -0500

    Add very basic REST methods for todomvc using in-memory list

--- NServiceMVC.Examples.Todomvc/Controllers/TodosController.cs
+++ NServiceMVC.Examples.Todomvc/Controllers/TodosController.cs
@@ -3,23 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using NServiceMVC;
 using AttributeRouting;
+using System.ComponentModel;
 
 namespace NServiceMVC.Examples.Todomvc.Controllers
 {
     public class TodosController : ServiceController
     {
+        private static IList<Models.Todo> Todos; // in-memory static list. Note no thread-safety!
+        public TodosController()
+        {
+            if (Todos == null)
+                Todos = new List<Models.Todo>(); // initialize the list if it's not already
+        }
+
         [GET("todos")]
+        [Description("List all Todos")]
         public IEnumerable<Models.Todo> Index()
         {
-            return new List<Models.Todo>() {
-                new Models.Todo() {
-                    Text = "Item1",
-                },
-                new Models.Todo() {
-                    Text = "Item2",
-                    Done = true,
-                }
-            };
+            return Todos;
+        }
+
+        [POST("todos")]
+        [Description("Add a Todo")]
+        public Models.Todo Add(Models.Todo item)
+        {
+            Todos.Add(item);
+            return item;
+        }
+
+        [PUT("todos/{id}")]
+        [Description("Update an existing Todo")]
+        public object Update(Guid id, Models.Todo item)
+        {
+            var existing = (from t in Todos where t.Id == id select t).FirstOrDefault();
+            if (existing != null)
+                existing = item;
+            return null;
+        }
+
+        [DELETE("todos/{id}")]
+        [Description("Delete a Todo")]
+        public object Delete(Guid id)
+        {
+            var existing = (from t in Todos where t.Id == id select t).FirstOrDefault();
+            Todos.Remove(existing);
+            return null;
         }
     }
 }
